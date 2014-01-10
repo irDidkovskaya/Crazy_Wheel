2d11e1dad1d434c701a81707fa2e8f854ed05e4f
Irina Didkovskaya <irdidkovskaya@gmail.com>
2014-01-10 15:56:31 +0200

    Initial Commit

---
+++ Wheely_Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Wheely_Test
+//
+//  Created by Irina Didkovskaya on 1/10/14.
+//  Copyright (c) 2014 Irina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Wheely_Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Wheely_Test
+//
+//  Created by Irina Didkovskaya on 1/10/14.
+//  Copyright (c) 2014 Irina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
